afc163250316645540894ce60a08e6c8aa57aadb
Samir Khanal <gdssamir@gmail.com>
2024-10-23 21:42:31 +0545

    home page UI complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { Login } from "./pages";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // <div className="w-screen h-screen flex flex-col items-center justify-center border-2 flex-wrap ">
+    //   <Button>Hey</Button>
+    //   <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    //     Taxing Laughter: The Joke Tax Chronicles
+    //   </h1>
+    //   <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    //     The People of the Kingdom
+    //   </h2>{" "}
+    //   <p className="leading-7 [&:not(:first-child)]:mt-6">
+    //     The king, seeing how much happier his subjects were, realized the error
+    //     of his ways and repealed the joke tax.
+    //   </p>
+    //   <p className="text-sm text-muted-foreground">Enter your email address.</p>
+    // </div>
+    <div>
+      <Login />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,130 @@
+import { FaXTwitter } from "react-icons/fa6";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const Login = () => {
+  const footerLinks = [
+    "About",
+    "Download the X app",
+    "Help Center",
+    "Terms of Service",
+    "Privacy Policy",
+    "Cookie Policy",
+    "Accessibility",
+    "Ads info",
+    "Blog",
+    "Careers",
+    "Brand Resources",
+    "Advertising",
+    "Marketing",
+    "X for Business",
+    "Developers",
+    "Directory",
+    "Settings",
+  ];
+  return (
+    <div className="w-full h-full min-h-screen bg-black text-white font-sans">
+      <div className="flex flex-col sm:flex-row gap-10 sm:justify-around p-10">
+        <div className="sm:w-full  sm:flex sm:justify-center sm:items-center">
+          <FaXTwitter className="text-5xl sm:text-[20rem]" />
+        </div>
+
+        <div className="sm:w-full sm:p-10">
+          <h1 className="text-4xl font-extrabold sm:text-7xl tracking-tight">
+            Happening now
+          </h1>
+          <div className=" mt-10">
+            <h2 className=" sm:text-3xl text-2xl font-semibold tracking-tight first:mt-0">
+              Join today.
+            </h2>
+
+            <Button className="w-72 h-12 mt-5 bg-blue-500 font-bold rounded-3xl">
+              Create Account
+            </Button>
+            <p className="w-72 mt-3 text-xs text-muted-foreground">
+              By signing up, you agree to the{" "}
+              <span className="text-blue-700">Terms of Service </span>and
+              <span className="text-blue-700"> Privacy Policy</span>, including{" "}
+              <span className="text-blue-700">Cookie Use</span>.
+            </p>
+          </div>
+
+          <div className="mt-12 font-bold sm:text-2xl">
+            <p>Already have an account?</p>
+
+            <Dialog>
+              <DialogTrigger>
+                <Button
+                  variant="outline"
+                  className="w-72 h-12 mt-5 font-semibold rounded-3xl text-blue-700 bg-black"
+                >
+                  Sign in
+                </Button>
+              </DialogTrigger>
+
+              <DialogContent className=" bg-black text-white rounded-md">
+                <DialogHeader>
+                  <DialogTitle>
+                    <FaXTwitter />
+                  </DialogTitle>
+                  <DialogDescription className="sm:text-2xl text-xl font-bold text-center text-white">
+                    Login
+                  </DialogDescription>
+                </DialogHeader>
+
+                <form className="w-full p-4 flex flex-col gap-5 flex-wrap justify-center">
+                  <div>
+                    <Label>
+                      <p className="text-lg font-semibold">
+                        Enter Email or Username:
+                      </p>
+                      <Input
+                        className="bg-black"
+                        placeholder="Email or Username"
+                      />
+                    </Label>
+                  </div>
+                  <div>
+                    <Label>
+                      <p className="text-lg font-semibold">Enter Password:</p>
+                      <Input className="bg-black" placeholder="Password" />
+                    </Label>
+                  </div>
+                  <Button
+                    type="submit"
+                    className="w-full rounded-3xl mx-auto  bg-white text-black"
+                  >
+                    Login
+                  </Button>
+                  <p className="text-center  text-muted-foreground">
+                    Don{`'`}t have an account?{" "}
+                    <span className="text-blue-700">Sign up</span>
+                  </p>
+                </form>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+      </div>
+      <p className="px-10 py-2 flex flex-wrap flex-grow gap-x-2 gap-y-1 text-xs sm:text-sm sm:justify-center text-gray-500">
+        {footerLinks.map((link, index) => (
+          <a key={index} href="#" className="hover:underline">
+            {link}
+          </a>
+        ))}
+        <span>© 2024 Samir Khanal.</span>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
