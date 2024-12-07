c7bde4e323535d22163982238248c70a816a4a68
Samir Khanal <gdssamir@gmail.com>
2024-12-07 09:06:35 +0545

    vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://social-media-0ebq.onrender.com",
       },
     },
   },
