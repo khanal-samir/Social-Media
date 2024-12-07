3775866c6ef9711943e35c20e577323e7eef9de9
Samir Khanal <gdssamir@gmail.com>
2024-12-07 09:33:19 +0545

    serverjs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
-import connectDB from "../db/index.js";
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
 });
-import { app } from "../app.js";
+import { app } from "./app.js";
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 5000, () => {
