cb8b0a437140764ac1eb982f85cec1e5d7e7fe15
Samir Khanal <gdssamir@gmail.com>
2024-12-22 14:38:51 +0545

    healthcheck

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,8 +22,10 @@ import followRouter from "./routes/follower.route.js";
 import commentRouter from "./routes/comment.route.js";
 import likeRouter from "./routes/like.route.js";
 import notificationRouter from "./routes/notification.route.js";
+import healthcheckRouter from "./routes/healthCheck.route.js";
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/tweet", tweetRouter);
 app.use("/api/v1/follow", followRouter);
 app.use("/api/v1/comment", commentRouter);
